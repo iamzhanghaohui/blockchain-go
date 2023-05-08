4237f61581cf83300455d0214eba13f4eeb45071
iamzhanghaohui <939026891@qq.com>
2023-05-08 16:23:32 +0800

    feat(): 持久化区块链

--- blockchain.go
+++ blockchain.go
@@ -1,18 +1,143 @@
 package main
 
-// Blockchian的数据结构
+import (
+	"fmt"
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+
+// tip 这个词本身有事物尖端或尾部的意思，这里指的是存储最后一个块的哈希
+// 在链的末端可能出现短暂分叉的情况，所以选择 tip 其实也就是选择了哪条链
+// db 存储数据库连接
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
-// 添加新的块
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
-}
-
-// NewBlockchain 用创世块开一条新的链
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	// 打开一个 BoltDB 文件
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+	//这是打开一个 BoltDB 文件的标准做法。注意，即使不存在这样的文件，它也不会返回错误。
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		// 如果数据库中不存在区块链就创建一个，否则直接读取最后一个块的哈希
+		if b == nil {
+			fmt.Println("No existing blockchain found. Creating a new one...")
+			genesis := NewGenesisBlock()
+
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put([]byte("l"), genesis.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
+}
+
+// 加入区块时，需要将区块持久化到数据库中
+func (bc *Blockchain) AddBlock(data string) {
+	var lastHash []byte
+
+	// 首先获取最后一个块的哈希用于生成新块的哈希
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte("l"), newBlock.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
+}
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
+}
+
+// 返回链中的下一个块
+/**
+注意，迭代器的初始状态为链中的 tip，
+因此区块将从尾到头（创世块为头），
+也就是从最新的到最旧的进行获取。实际上，
+选择一个 tip 就是意味着给一条链“投票”。
+一条链可能有多个分支，最长的那条链会被认为是主分支。
+在获得一个 tip （可以是链中的任意一个块）之后，我们就可以重新构造整条链，
+找到它的长度和需要构建它的工作。这同样也意味着，一个 tip 也就是区块链的一种标识符。
+*/
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go-blockchain
 
 go 1.17
+
+require github.com/boltdb/bolt v1.3.1
+
+require golang.org/x/sys v0.8.0 // indirect
